2486b965827fe1054c6427c07d4766f862a024f3
Stephen Furbank <furbank@hotmail.com>
2024-12-01 06:25:36 +0000

    setup day 1 part 1

---
+++ day-01/src/part-1.rs
@@ -0,0 +1,23 @@
+#![allow(unused)]
+
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(_input:&str) -> String {
+    "todo!()".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    //use crate::part1;
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let result = part1("");
+        assert_eq!(result, "4".to_string());
+    }
+}
