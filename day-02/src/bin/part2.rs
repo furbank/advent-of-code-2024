e2bfaad2436ab31ce389455cc47d2e178f40fd70
Stephen Furbank <furbank@hotmail.com>
2024-12-02 22:39:14 +0000

    D2P2: Working

--- day-02/src/bin/part2.rs
+++ day-02/src/bin/part2.rs
@@ -1,4 +1,3 @@
-#![allow(unused)]
 use std::str::FromStr;
 use std::iter::zip;
 
@@ -19,36 +18,44 @@ fn part1(input:&str) -> String {
         .collect();
 
     for report in rows {
-        if is_safe(&report){total_safe += 1}
+        if test_report(&report) == [] { total_safe += 1 }
+        else if brute_fix( &report ){ total_safe +=1 }
     }
-
     total_safe.to_string()
 }
 
-fn is_safe(report:&Vec<i32>) -> bool {
+fn test_report(report:&Vec<i32>) -> Vec<usize> {
     // Rules
     // The levels are either all increasing or all decreasing.
     // Any two adjacent levels differ by at least one and at most three.
     let level_diffs: Vec<i32> = find_diffs(&report);
+    let mut location: usize = 0;
+    let mut problem_locations: Vec<usize> =Vec::new();
 
     for change in &level_diffs {
         match &level_diffs[0] {
             1..=3 => {
                 match change {
+                    0 => problem_locations.push(location),
                     1..=3 => {},
-                    _ => return false
+                    4.. => problem_locations.push(location),
+                    _ => problem_locations.push(location)
                     }
             },
             -3..=-1 => {
                 match change {
+                    0 => problem_locations.push(location),
                     -3..=-1 => {},
-                    _ => return false
-                    }
+                    ..-3 => problem_locations.push(location),
+                    _ => problem_locations.push(location)
+                }
             },
-            _ => return false
-        }
+            _ => problem_locations.push(location)
     }
-    true
+
+        location += 1;
+    }
+    problem_locations
 }
 
 fn find_diffs(report: &Vec<i32>) -> Vec<i32> {
@@ -56,4 +63,13 @@ fn find_diffs(report: &Vec<i32>) -> Vec<i32> {
         .into_iter()
         .map(|(a,b)| (a - b))
         .collect::<Vec<i32>>()
+}
+
+fn brute_fix(report: &Vec<i32>) -> bool {
+    for i in 0..report.len(){
+        let try_fix_report: &mut Vec<i32> = &mut report.clone();
+        try_fix_report.remove(i);
+        if test_report(try_fix_report) == []{ return true };
+    }
+    false
 }
