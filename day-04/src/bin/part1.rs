bd34409defe410c4d1e0a50fd394a7f85de9934a
Stephen Furbank <furbank@hotmail.com>
2024-12-04 17:04:44 +0000

    D4P1: Start

---
+++ day-04/src/bin/part1.rs
@@ -0,0 +1,12 @@
+#![allow(unused)]
+
+fn main() {
+    let input = include_str!("../input");
+    let output = part1(input);
+    println!("{:?}", &input);
+    dbg!(output);
+}
+
+fn part1(_input:&str) -> String {
+    "todo!()".to_string()
+}
